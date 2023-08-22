112c7761a23d677f957895d17ba44b8b7aa8f52a
Eric Haupt <ehaupt2@huskers.unl.edu>
2023-08-22 09:53:54 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Eric Haupt
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Eric Haupt\n");
+
+  return 0;
+}
